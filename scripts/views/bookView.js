a7499ce0a38e72608e5d15c2c4b92a383adbf4e0
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-13 15:26:35 -0800

    basic init functions for new pages

--- scripts/views/bookView.js
+++ scripts/views/bookView.js
@@ -15,6 +15,29 @@ var __API_URL__ = 'http://localhost:3000';
     bookView.initIndexPage = () => {
         bookView.toggleIndex();
     }
+
+    bookView.initDetailPage = function(ctx) {
+        $('.body-container').hide();
+        $('.book-description').show();
+        $('.book-detail').empty();
+        let template = Handlebars.compile($('#book-detail-template').text());
+        $('.book-detail').append(template(ctx));
+    }
+
+    bookView.initCreateFormPage = function() {
+        $('.body-container').hide();
+        $('.book-new').show();
+        $('#new-book-form').on('submit', function(event){
+            event.preventDefault();
+            let book = {
+                title: event.target.title.value,
+                author: event.target.author.value,
+                isbn: event.target.isbn.value,
+                url: event.target.url.value,
+                description: event.target.description.value
+            }
+        })
+    }
     
     module.bookView = bookView; // keep at bottom
 })(app);
