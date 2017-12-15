2bf8e31339ca623c4bff6d5371ef3c24d186b711
svivek71@yahoo.com <svivek71@yahoo.com>
2017-12-14 18:31:59 -0800

    working admin

--- scripts/views/menuView.js
+++ scripts/views/menuView.js
@@ -7,10 +7,8 @@ var __API_URL__ = 'http://localhost:3000';
     var menuView = {};
 
     menuView.listenMenu = () => {
-        console.log('listening in listenMenu');
         $('.main-nav').on('click', function(e){
             $('.main-nav ul').toggle();
-            console.log('element toggled');
         });
     }
     module.menuView = menuView; 
